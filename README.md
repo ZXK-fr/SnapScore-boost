966710ecc8944eed9f50989ac497b29b0de17b2b
zxk <lpro9068@gmail.com>
2025-04-15 22:09:26 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,52 @@
+<br/>
+<div align="center">
+  <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c536.png" alt="Logo" width="120" height="120">
+
+  <h2 align="center">SnapScore Boost</h2>
+
+  <p align="center">
+    boost ton snapscore automatiquement et gratuitement avec ce script développé par zxk
+  </p>
+</div>
+
+---
+
+![screenshot](https://i.ytimg.com/vi/YBJ0_TYDDUk/maxresdefault.jpg)
+
+---
+
+## ⚙️ comment utiliser
+
+### windows
+
+1. installe python 3.9.2  
+2. coche **add python to PATH** pendant l’installation  
+3. lance le fichier `Install Requirements.bat`  
+4. installe un émulateur android (genre NoxPlayer)  
+5. ouvre snapchat sur l’émulateur et connecte ton compte  
+6. crée un raccourci avec les gens à qui tu veux envoyer les snaps  
+7. lance `main.py` avec python  
+8. suis les instructions dans le terminal  
+
+---
+
+### linux
+
+1. ouvre ton terminal  
+2. clone le repo : `git clone https://github.com/ZXK-fr/SnapScore-Boost`  
+3. va dans le dossier : `cd SnapScore-Boost`  
+4. installe les dépendances : `pip install -r requirements.txt`  
+5. installe `scrcpy` pour contrôler ton tel android  
+6. connecte ton téléphone en USB ou WiFi  
+7. ouvre snapchat et fais un raccourci avec les gens  
+8. lance le script : `sudo python main.py`  
+9. suis les instructions  
+
+---
+
+## ⚠️ à savoir
+
+- laisse tourner ton pc pendant que le script envoie les snaps  
+- c’est un projet éducatif uniquement  
+- t’es responsable de ce que t’en fais, pas moi  
+
---
+++ main.py
@@ -0,0 +1,138 @@
+try:
+    from colorama import Fore, init
+    import ctypes, pyautogui, keyboard, os, time, platform
+    from datetime import datetime
+    from pystyle import Colors, Colorate
+except ImportError:
+    input("Erreur : installe les modules du fichier requirements.txt")
+
+init(autoreset=True)
+
+ascii_text = Fore.YELLOW + """
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣶⣶⣶⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⡀⠀⠀⠀
+⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀    
+⠀⠀⠀⠈⠙⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠋⠁⠀⠀⠀
+⠀⠀⠀⣀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀
+⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄
+⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃
+⠀⠀⠉⠉⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠉⠉⠀⠀
+⠀⠀⠀⠀⠈⠛⠛⠋⠙⠿⣿⣿⣿⣿⣿⣿⠿⠋⠙⠛⠛⠁⠀⠀⠀
+"""
+
+dedi = (Colorate.Horizontal(Colors.green_to_black, "SnapScore boost by Zxk"))
+def sur_linux():
+    return platform.system() == "Linux"
+
+class snapchat:
+
+    def __init__(self):
+        self.snaps_envoyes = 0
+        self.delai = 0.3
+        self.debut = None
+
+    def choper_positions(self):
+        self.log("mets ta souris sur *caméra* et appuie sur F")
+        self.bouton_camera = self.attendre_f()
+
+        self.log("mets ta souris sur *prendre photo* et appuie sur F")
+        self.bouton_photo = self.attendre_f()
+
+        self.log("mets ta souris sur *envoyer à* et appuie sur F")
+        self.bouton_envoyer_a = self.attendre_f()
+
+        self.log("mets ta souris sur ton *raccourci* et appuie sur F")
+        self.raccourci = self.attendre_f()
+
+        self.log("mets ta souris sur *tout sélectionner* et appuie sur F")
+        self.selectionner_tout = self.attendre_f()
+
+        self.log("mets ta souris sur *envoyer snap* et appuie sur F")
+        self.bouton_envoyer_snap = self.attendre_f()
+
+    def attendre_f(self):
+        while True:
+            if keyboard.is_pressed("f"):
+                pos = pyautogui.position()
+                time.sleep(0.5)
+                return pos
+
+    def envoyer_snap(self, nb_personnes):
+        self.mettre_titre(nb_personnes)
+        pyautogui.moveTo(self.bouton_camera)
+        pyautogui.click()
+        time.sleep(self.delai)
+
+        pyautogui.moveTo(self.bouton_photo)
+        pyautogui.click()
+        time.sleep(self.delai)
+
+        pyautogui.moveTo(self.bouton_envoyer_a)
+        pyautogui.click()
+        time.sleep(self.delai)
+
+        pyautogui.moveTo(self.raccourci)
+        pyautogui.click()
+        time.sleep(self.delai)
+
+        pyautogui.moveTo(self.selectionner_tout)
+        pyautogui.click()
+        time.sleep(self.delai)
+
+        pyautogui.moveTo(self.bouton_envoyer_snap)
+        pyautogui.click()
+
+        self.snaps_envoyes += 1
+        self.mettre_titre(nb_personnes)
+
+    def mettre_titre(self, nb_personnes):
+        maintenant = time.time()
+        passe = str(maintenant - self.debut).split(".")[0]
+        total = self.snaps_envoyes * nb_personnes
+        if not sur_linux():
+            ctypes.windll.kernel32.SetConsoleTitleW(
+                f"SnapScore boost | Snaps envoyés : {total} | Temps : {passe}s | Dev : Zxk"
+            )
+
+    def log(self, txt):
+        print(f"{Fore.BLUE}\n[{Fore.MAGENTA}Console{Fore.BLUE}] {txt}")
+
+    def lancer(self):
+        os.system("cls" if not sur_linux() else "clear")
+        print(ascii_text)
+        print(dedi)
+        self.choper_positions()
+
+        while True:
+            try:
+                nb_personnes = int(input(f"\n       {Fore.CYAN}[{Fore.MAGENTA}Console{Fore.CYAN}] combien de gens dans le raccourci ? "))
+                break
+            except ValueError:
+                self.log("erreur, entre un vrai chiffre")
+
+        self.log("PC lent = tape 1")
+        self.log("PC rapide = tape 2")
+        choix = input(f"\n       {Fore.CYAN}[{Fore.MAGENTA}Console{Fore.CYAN}] ton choix : ")
+        if choix == "1":
+            self.delai = 2
+
+        self.log("va dans les chats et appuie sur F quand t’es prêt")
+        while not keyboard.is_pressed("f"):
+            pass
+
+        os.system("cls" if not sur_linux() else "clear")
+        print(ascii_text)
+        self.log("ça envoie...")
+        self.debut = time.time()
+
+        while not keyboard.is_pressed("F4"):
+            self.envoyer_snap(nb_personnes)
+            time.sleep(4)
+
+        self.log(f"fini d’envoyer {self.snaps_envoyes} snaps")
+
+bot = snapchat()
+bot.lancer()
